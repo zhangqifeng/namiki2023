e0b1a38896d50d8481919e53b5a416bc6a88cde9
Ninox4211 <18583007407@163.com>
2023-12-20 10:40:25 +0900

    打卡表验证
    okay
    `

--- src/main/java/com/thymeleaf/config/MvcConfig.java
+++ src/main/java/com/thymeleaf/config/MvcConfig.java
@@ -7,14 +7,16 @@ import org.springframework.web.servlet.config.annotation.*;
 public class MvcConfig implements WebMvcConfigurer {
     @Override
     public void addViewControllers(ViewControllerRegistry registry) {
-        registry.addViewController("login").setViewName("login");
-        registry.addViewController("regist").setViewName("regist");
-        registry.addViewController("addEmp").setViewName("addEmp");
-        registry.addViewController("emplist").setViewName("emplist");
-        registry.addViewController("updateEmp").setViewName("updateEmp");
-        registry.addViewController("employeelogin").setViewName("employeelogin");
-        registry.addViewController("attendance").setViewName("attendance");
-        registry.addViewController("clock").setViewName("clock");
+
+       registry.addViewController("login").setViewName("login");
+       registry.addViewController("regist").setViewName("regist");
+       registry.addViewController("addEmp").setViewName("addEmp");
+       registry.addViewController("emplist").setViewName("emplist");
+       registry.addViewController("updateEmp").setViewName("updateEmp");
+       registry.addViewController("employeelogin").setViewName("employeelogin");
+       registry.addViewController("attendance").setViewName("attendance");
+       registry.addViewController("clock").setViewName("clock");
+
     }
 
 }
